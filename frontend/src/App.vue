190b28d95c7cf8a1bf7b22ca097c418ac73249b3
Jéssica Paiva <jessica.irispaiva@gmail.com>
2026-04-22 15:11:08 +0100

    HomePage

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import PageContainer from './components/PageContainer.vue';
+import HomeScreen from './paths/HomeScreen.vue';
+</script>
+
+<template>
+  <PageContainer>
+    <HomeScreen />
+  </PageContainer>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/paths/HomeScreen.vue
@@ -0,0 +1,40 @@
+<script></script>
+
+<template>
+    <div class="container">
+        <div class="infoBox"></div>
+        <div class="optionsRow">
+            <div><i class="bi bi-cash-coin"></i></div>
+            <div><i class="bi bi-piggy-bank"></i></div>
+        </div>
+    </div>
+</template>
+
+<style>
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center; /* vertical */
+    align-items: center;     /* horizontal */
+    height: 100vh;
+    gap: 20px;
+}
+
+.container div {
+    padding: 10px;
+}
+
+.optionsRow {
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
+}
+
+.infoBox {
+    width: 800px;
+    height: 350px;
+    border-radius: 8px;
+    background-color: white;
+    box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+}
+</style>
