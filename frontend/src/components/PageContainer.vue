8a5c0a29573f28c21602cf6d045d279a3d20be9a
Jéssica Paiva <jessica.irispaiva@gmail.com>
2026-04-22 15:42:24 +0100

    Starting Responsive Layout (HomePage)

--- frontend/src/components/PageContainer.vue
+++ frontend/src/components/PageContainer.vue
@@ -7,5 +7,6 @@
 <style scoped>
 .container {
   width: 100%;
+  height: 100%;
 }
 </style>
--- frontend/src/paths/HomeScreen.vue
+++ frontend/src/paths/HomeScreen.vue
@@ -4,8 +4,8 @@
     <div class="container">
         <div class="infoBox"></div>
         <div class="optionsRow">
-            <div><i class="bi bi-cash-coin"></i></div>
-            <div><i class="bi bi-piggy-bank"></i></div>
+            <button class="button1"><i class="bi bi-cash-coin"></i></button>
+            <button class="button1"><i class="bi bi-piggy-bank"></i></button>
         </div>
     </div>
 </template>
@@ -31,8 +31,9 @@
 }
 
 .infoBox {
-    width: 800px;
-    height: 350px;
+    aspect-ratio: 1/2;
+    width: 50%;
+    /* height: 350px; */
     border-radius: 8px;
     background-color: white;
     box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
