e6c04169d39930baf851462ba533558277c8ad69
Jéssica Paiva <jessica.irispaiva@gmail.com>
2026-04-27 10:46:43 +0100

    SavingsScreen

--- frontend/src/paths/SavingsScreen.vue
+++ frontend/src/paths/SavingsScreen.vue
@@ -1,5 +1,20 @@
 <script setup></script>
 
-<template>Savings Screen</template>
+<template>
+    <div class="container">
+        <button class="button2">Year</button>
+        <button class="button2">Monthly View</button>
+    </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    width: 100%;
+    height: 100%;
+}
+</style>
